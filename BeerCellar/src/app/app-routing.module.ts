de9569da329911b6c7336f60c0751d3ca1661301
cwcheung <carl.w.cheung@gmail.com>
2019-12-19 23:32:44 -0500

    add logout route

--- BeerCellar/src/app/app-routing.module.ts
+++ BeerCellar/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component'; 
+import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
   {path: '', component: UserComponent}, 
   {path: 'addUser', component: AddUserComponent}, 
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'logout', component: LogoutComponent}
 ];
 
 @NgModule({
