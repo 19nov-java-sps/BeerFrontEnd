4898e0ac900d23c98ac29fcc6f3a29b94723c5e6
cwcheung <carl.w.cheung@gmail.com>
2019-12-19 22:13:22 -0500

    add authentification service

--- BeerCellar/src/app/services/authentification.service.ts
+++ BeerCellar/src/app/services/authentification.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators'; 
 import { environment } from '@environments/environment'; 
 import { User } from './models'; 
@@ -19,5 +19,20 @@ export class AuthentificationService {
 
   public get currentUserValue(): User {
     return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, {username, password}).pipe(map(user => {
+      if (user && user.token) {
+        localStorage.setItem('currentUser', JSON.stringify(user)); 
+        this.currentUserSubject.next(user); 
+      }
+      return user;
+    }));
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser'); 
+    this.currentUserSubject.next(null); 
   }
 }
