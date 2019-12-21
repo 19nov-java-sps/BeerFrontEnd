5d127a7dd20d7f175b147e8cf66a8046afe90321
cwcheung <carl.w.cheung@gmail.com>
2019-12-20 23:56:31 -0500

    add login component and service

---
+++ BeerCellar/src/app/services/login.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
+import { Router } from '@angular/router'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private httpClient: HttpClient, private router: Router) { }
+
+  private url: string = "http://localhost:8080/BeerBackEnd/login"; 
+  private body: string; 
+
+  private httpOptions = {
+    headers: new HttpHeaders({"Content-Type": "application/x-www-form-urlencoded"}), 
+    observe: "response" as "body"
+  }; 
+
+  private auth: string;
+
+  authenticate(username: string, password: string): void {
+    this.body = `username=${username} & password=${password}`; 
+    this.http.post(`${this.url}`, this.body, this.httpOptions).subscribe(
+      response => {
+        this.auth = (response["headers"].get("Authorization")); 
+        sessionStorage.setItem("token", this.auth); 
+        this.router.navigate([]);
+      }
+    );
+  }
+
+}
