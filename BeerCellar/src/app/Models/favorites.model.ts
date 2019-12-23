3c2e718590d2d60e39e9f0ed95f0a115687e413a
cwcheung <carl.w.cheung@gmail.com>
2019-12-23 11:34:00 -0500

    add favorites component and service

---
+++ BeerCellar/src/app/Models/favorites.model.ts
@@ -0,0 +1,5 @@
+export class Favorite {
+  name?: string; 
+  id?: string;
+  user: string; 
+}
--- BeerCellar/src/app/services/favorites.service.ts
+++ BeerCellar/src/app/services/favorites.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@angular/core';
-// import { HttpClient, HttpHeaders } from '@angular/common/http'; 
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
 // import { Router } from '@angular/router'; 
+import { Observable } from 'rxjs'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  private token = sessionStorage.getItem("token"); 
+
+  private apiUrl: string = 'https://sandbox-api.brewerydb.com/v2/beer/random/?key=0da93aab15976e9da7c3265b3d4f486f'; 
+  private url: string = 'http:/localhost:8080/BeerBackend/user/favorites'; 
+
+  getBeerId(): Observable<any> {
+    return this.httpClient.get(); 
+  }
+
+  addFavoriteBeer(beerId: number): Observable<any> {
+    return this.httpClient.get();
+  }
 }
+
+
+
+
+//cwc 22 Dec
